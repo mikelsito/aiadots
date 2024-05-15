481daa33c06862432d3794ec8c48f0fb7a834879
ibrah100 <37915317+ibrah100@users.noreply.github.com>
2024-05-15 17:42:45 -0500

    fixed css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,8 @@ module.exports = {
       colors: {
         gfblack: "#030303",
         gfred: "#912411",
-        gfgold: "C6A25A",
-        gfbrown: "A46329",
+        gfgold: "#C6A25A",
+        gfbrown: "#A46329",
         gfgrey: "#D3CFC3",
       },
     },
